2e315f9906520871440d267a8c5602bed365eb80
Najam us saqib <najamkhan89@gmail.com>
2021-08-13 00:43:54 +0500

    label added

--- src/components/EthereumAddress.js
+++ src/components/EthereumAddress.js
@@ -2,13 +2,16 @@ import { Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import React from "react";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   msgContainer: {
     marginTop: "2.5rem",
     marginBottom: "1.5rem",
-    textAlign: "center",
   },
-});
+  msgTitle: {
+    color: theme.palette.primary.main,
+    marginRight: "4px",
+  },
+}));
 
 function EthereumAddress({ successData, setStep }) {
   const classes = useStyles();
@@ -18,9 +21,18 @@ function EthereumAddress({ successData, setStep }) {
   return (
     <div>
       <div className={classes.msgContainer}>
-        <Typography variant="subtitle2">{successData.address}</Typography>
-        <Typography variant="subtitle2">{successData.txid}</Typography>
-        <Typography variant="subtitle2">{successData.amount}</Typography>
+        <Typography variant="subtitle2">
+          <span className={classes.msgTitle}>Address:</span>
+          {successData.address}
+        </Typography>
+        <Typography variant="subtitle2">
+          <span className={classes.msgTitle}>Txid:</span>
+          {successData.txid}
+        </Typography>
+        <Typography variant="subtitle2">
+          <span className={classes.msgTitle}>Amount:</span>
+          {successData.amount}
+        </Typography>
       </div>
       <Button variant="contained" color="primary" onClick={handleClick}>
         Add more Token
